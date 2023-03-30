e0b17bff1f4600819e481457df753aaff6823d1f
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-30 16:18:20 +0700

    Menampilkan comparasions dan movements

--- QuickSort_3nim/QuickSort_3nim.cpp
+++ QuickSort_3nim/QuickSort_3nim.cpp
@@ -93,11 +93,4 @@ void display() {
     cout << "Number of data movements : " << mov_count << endl;
 }
 
-int main()
-{
-    input();
-    q_short(0, n - 1);
-    display();
-    system("pause");
-    return 0;
 }
