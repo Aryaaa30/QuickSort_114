0523720cab1cd7413cadd85e2c30ab251272b671
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-30 16:11:38 +0700

    Memasukkan  Bilangan

--- QuickSort_3nim/QuickSort_3nim.cpp
+++ QuickSort_3nim/QuickSort_3nim.cpp
@@ -84,6 +84,13 @@ void display() {
     cout << "Sorted Array" << endl;
     cout << "\n==========" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasions : " << cmp_count << endl;
+    cout << "Number of data movements
 }
 
 int main()
