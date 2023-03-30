24c04ca07ecc099283bc9941180341540700b558
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-30 16:05:47 +0700

    Menampilkan Prosedur Sorted Array

--- QuickSort_3nim/QuickSort_3nim.cpp
+++ QuickSort_3nim/QuickSort_3nim.cpp
@@ -79,6 +79,13 @@ void q_short(int low, int high)
     }
 }
 
+void display() {
+    cout << "\n==========" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "\n==========" << endl;
+
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
