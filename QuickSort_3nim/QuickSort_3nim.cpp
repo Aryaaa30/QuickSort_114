a41300151b7690569a7c8a9a340c3e1218bfec7b
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-30 15:11:30 +0700

    Prosedur Input

--- QuickSort_3nim/QuickSort_3nim.cpp
+++ QuickSort_3nim/QuickSort_3nim.cpp
@@ -6,6 +6,21 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukkan Panjang Element Array : ";
+        cin >> n;
+        
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum Panjang Array Adalah 20" << endl;
+    }
+    cout << "\n===================" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n===================" << endl;
+}
 
 int main()
 {
